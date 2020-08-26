05b42ffdfac79720204628f7f1fe25cd45eb7eef
Jedediah Arnold <jarnold910@gmail.com>
2020-08-25 22:49:46 -0400

    scaffold the blogly - db conected

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 """Blogly application."""
 
-from flask import Flask
+from flask import Flask, render_template
 from models import db, connect_db
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = "mysecretkey"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+debug = DebugToolbarExtension(app)
+
 connect_db(app)
 db.create_all()
+
+
+@app.route('/')
+def get_home():
+    return render_template('home.html')
