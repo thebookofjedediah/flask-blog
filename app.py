7fe1aa24eb8449bf4a9f3edca7b4d4383331703b
Jedediah Arnold <jarnold910@gmail.com>
2020-08-26 00:00:33 -0400

    viewing users and detailed users

--- app.py
+++ app.py
@@ -1,23 +1,35 @@
 """Blogly application."""
 
 from flask import Flask, render_template
-from models import db, connect_db
+from models import db, connect_db, User
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
-
 
 app.config['SECRET_KEY'] = "mysecretkey"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
 connect_db(app)
-db.create_all()
 
 
 @app.route('/')
-def get_home():
-    return render_template('home.html')
+def get_users():
+    """shows list of all users"""
+    users = User.query.all()
+    return render_template('list.html', users=users)
+
+@app.route('/<int:user_id>')
+def user_details(user_id):
+    """show details about user"""
+    user = User.query.get(user_id)
+    return render_template('user-details.html', user=user)
+
+@app.route('/add-user')
+def get_user_form():
+    """show form to add a user"""
+    return render_template("user-form.html")
--- models.py
+++ models.py
@@ -7,3 +7,18 @@ def connect_db(app):
     db.init_app(app)
 
 
+# Models Here
+
+class User(db.Model):
+    def __repr__(self):
+        """show info about user"""
+        u = self
+        return f"<User id={u.id} first_name={u.first_name} last_name={u.last_name} image_url={u.image_url}>"
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, nullable=False, default="https://cloudcone.com/wp-content/uploads/2019/03/blank-avatar.jpg")
+
